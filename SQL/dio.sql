dfa411dce5d94f10e9fa444dc6b55d1b6b1ad059
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-03-04 16:39:15 -0300

    Atualizando os conteudos

--- SQL/dio.sql
+++ SQL/dio.sql
@@ -1,13 +1,26 @@
-CREATE TABLE pessoas(
+CREATE TABLE pessoa( /* Criamos uma tabela */
     id INT NOT NULL PRIMARY KEY AUTO INCREMENT, 
     nome VARCHAR(40) NOT NULL,
     nasc DATE
 )
 
-INSERT INTO pessoas (nome, nasc) VALUES ('PEDRO', '1997-10-08')
-INSERT INTO pessoas (nome, nasc) VALUES ('FERNANDA', '1997-06-01')
-INSERT INTO pessoas (nome, nasc) VALUES ('MARTA', '1997-02-07')
-INSERT INTO pessoas (nome, nasc) VALUES ('CESAR', '1997-01-28')
-INSERT INTO pessoas (nome, nasc) VALUES ('LUCAS', '1997-08-18')
-INSERT INTO pessoas (nome, nasc) VALUES ('AMANDA', '1997-11-13')
-INSERT INTO pessoas (nome, nasc) VALUES ('GUILHERME', '1997-12-08')
+/*Inserimos dados na tabela*/
+INSERT INTO pessoa (nome, nasc) VALUES ('PEDRO', '1997-10-08');
+INSERT INTO pessoa (nome, nasc) VALUES ('FERNANDA', '1997-06-01');
+INSERT INTO pessoa (nome, nasc) VALUES ('MARTA', '1997-02-07');
+INSERT INTO pessoa (nome, nasc) VALUES ('CESAR', '1997-01-28');
+INSERT INTO pessoa (nome, nasc) VALUES ('LUCAS', '1997-08-18');
+INSERT INTO pessoa (nome, nasc) VALUES ('AMANDA', '1997-11-13'); 
+INSERT INTO pessoa (nome, nasc) VALUES ('GUILHERME', '1997-12-08');
+
+/* Atualizamos todos os nomes*/
+UPDATE pessoa SET nome='Pedro Gobis' WHERE id=2;
+UPDATE pessoa SET nome='Fernda Martins' WHERE id=3;
+UPDATE pessoa SET nome='Marta Suplicy' WHERE id=4;
+UPDATE pessoa SET nome='Cesar Rossi' WHERE id=5;
+UPDATE pessoa SET nome='Lucas Azevedo' WHERE id=6;
+UPDATE pessoa SET nome='Amanda Vasques' WHERE id=7;
+UPDATE pessoa SET nome='Guilherme Stracci' WHERE id=8;
+
+DELETE FROM pessoa WHERE id=3;
+/*matamos a marta*/
