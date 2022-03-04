cdfc26acd44dfb87d9476e97d4a7f4e54a55acfe
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-03-04 16:14:14 -0300

    Aula mysql

---
+++ SQL/dio.sql
@@ -0,0 +1,13 @@
+CREATE TABLE pessoas(
+    id INT NOT NULL PRIMARY KEY AUTO INCREMENT, 
+    nome VARCHAR(40) NOT NULL,
+    nasc DATE
+)
+
+INSERT INTO pessoas (nome, nasc) VALUES ('PEDRO', '1997-10-08')
+INSERT INTO pessoas (nome, nasc) VALUES ('FERNANDA', '1997-06-01')
+INSERT INTO pessoas (nome, nasc) VALUES ('MARTA', '1997-02-07')
+INSERT INTO pessoas (nome, nasc) VALUES ('CESAR', '1997-01-28')
+INSERT INTO pessoas (nome, nasc) VALUES ('LUCAS', '1997-08-18')
+INSERT INTO pessoas (nome, nasc) VALUES ('AMANDA', '1997-11-13')
+INSERT INTO pessoas (nome, nasc) VALUES ('GUILHERME', '1997-12-08')
