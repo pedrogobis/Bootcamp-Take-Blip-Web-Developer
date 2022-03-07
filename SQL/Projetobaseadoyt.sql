da0a381e40ea886a1b2662c3d5c2203e532487bd
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-03-07 09:46:48 -0300

    Update Projetobaseadoyt.sql

--- SQL/Projetobaseadoyt.sql
+++ SQL/Projetobaseadoyt.sql
@@ -75,12 +75,65 @@ SELECT videos.title, autor.nome, seo.category FROM videos JOIN seo ON videos.fk_
 JOIN autor ON videos.fk_autor = autor.id_autor;
 
 
-
+/*A tabela de playlist vai se dividir em duas, pois tem muitos dados de fora. */
 CREATE TABLE playlist(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    videos
-    title VARCHAR(60)
+    id_playlist INT PRIMARY KEY AUTO_INCREMENT,
+    nome_pl VARCHAR(35)
 )
+
+/*Alimentando ela*/
+INSERT INTO playlist (nome_pl) VALUES ('Politicas atuais');
+INSERT INTO playlist (nome_pl) VALUES ('Diversão');
+INSERT INTO playlist (nome_pl) VALUES ('Receitas');
+
+/*Criando tabela de muitos para muitos.*/ 
+CREATE TABLE videos_playlist(
+    id_vp INT PRIMARY KEY AUTO_INCREMENT,
+    fk_videos INT,
+    fk_playlist INT
+)
+
+/*Inserimos dados de outras tabelas na propria tabela videos_playlist*/
+INSERT INTO videos_playlist (fk_videos, fk_playlist) VALUES(1,1);
+INSERT INTO videos_playlist (fk_videos, fk_playlist) VALUES(2,1);
+INSERT INTO videos_playlist (fk_videos, fk_playlist) VALUES(1,2);
+INSERT INTO videos_playlist (fk_videos, fk_playlist) VALUES(7,2);
+INSERT INTO videos_playlist (fk_videos, fk_playlist) VALUES(4,2);
+
+/*Visualizando os dados das tabelas.*/
+SELECT * FROM playlist JOIN videos_playlist ON playlist.id_playlist = videos_playlist.fk_playlist;
+
+/*
+Visualizando os dados das tabelas e mostrando os nomes dos videos com a tabela.
+
+LER:
+traga todas as informações da tabela playlist, junte com todos videoplaylist onde minha idplaylist de playlist vai ser igual a fk_playlist de videos playlist.
+junte com a tabela videos onde id de videos em videos, é igual a fk videos na table avideos_playlist
+
+*/
+SELECT * FROM playlist JOIN videos_playlist ON playlist.id_playlist = videos_playlist.fk_playlist
+JOIN videos ON videos.id_vid = videos_playlist.fk_videos;
+
+/*Aqui vamos mostrar apenas o nome da playlist e o nome do video*/
+SELECT playlist.nome_pl, videos.title FROM playlist JOIN videos_playlist ON playlist.id_playlist = videos_playlist.fk_playlist JOIN videos ON videos.id_vid = videos_playlist.fk_videos;
+
+/*Mostrar o nome do autor*/
+SELECT playlist.nome_pl, videos.title, autor.nome FROM playlist 
+JOIN videos_playlist ON playlist.id_playlist = videos_playlist.fk_playlist 
+JOIN videos ON videos.id_vid = videos_playlist.fk_videos
+JOIN autor ON videos.fk_autor = autor.id_autor;
+
+/*Vamos fazer uma alteração na tabela playlist, vamos colocar o nome do criador de uma playlist.*/
+
+ALTER TABLE playlist ADD fk_autor INT NOT NULL AFTER nome_pl;
+
+/*Vinculando dados*/
+UPDATE playlist SET fk_autor=5 WHERE id_playlist=1; /*SOSK COM PLAYLIST POLITICA*/
+UPDATE playlist SET fk_autor=6 WHERE id_playlist=2;/* TAIS COM PLAYLIST DIVERSAO*/
+UPDATE playlist SET fk_autor=1 WHERE id_playlist=3;/* MARTA COM PLAYLIST RECEITAS*/
+
+/* Mostrando nome do criador da playlist e o nome da playlist*/
+SELECT autor.nome, playlist.nome_pl FROM playlist JOIN autor ON playlist.fk_autor = autor.id_autor;
 
 /*Criada*/  
 CREATE TABLE SEO(
