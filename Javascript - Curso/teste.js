58e0c8fe53cd783a332979d542369b19afa8f89e
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-03-08 12:18:03 -0300

    JS DIO.
    começando as aulas de JS no bootcamp da dio

---
+++ Javascript - Curso/teste.js
@@ -0,0 +1,21 @@
+/*COMENTARIOS COMPLEXOS*/
+// Comentario simples.
+
+var desconto = 0.3;
+const PRECO = 10;
+var total = PRECO - desconto;
+
+console.log(total)
+
+var a = 3;
+var b = 10;
+
+function soma(a,b) {
+    console.log(`valor de a = ${a}, valor de b = ${b}`)
+    return a + b;
+
+
+}
+
+// Igual no python kdfgjsdçljkgdjlçk
+soma(a, b);
