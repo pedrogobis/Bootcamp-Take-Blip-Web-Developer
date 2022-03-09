2e403d107c6d73aa42eb5279a3e008a11e0ae0fc
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-03-09 14:34:36 -0300

    Quebrei a cabeça
    Tentei de tudo, e  minha to-do-list n funcionou por algum motivo, do jeito simples que estava fazendo o valor por algum motivo n era convertido.
    fui tentar fazer do jeito da aula tbm n rolou

--- Javascript - Curso/ex02- to-do-list/assets/js/js.js
+++ Javascript - Curso/ex02- to-do-list/assets/js/js.js
@@ -1,7 +1,7 @@
-const form = document.querySelector('#task-form')
-const res = document.querySelector('#res') 
-
-form.onsubmit = function(e){
+const form = document.getElementById('#task-form')
+const taskList = document.getElementById('#tasks') 
+/*
+form.onsubmit = function (e) {
     e.preventDefault();
     const inputField = document.getElementById('task-input');
     addTask(inputField.value);
@@ -10,19 +10,59 @@ form.onsubmit = function(e){
 }
 
 function addTask(description){
-    const taskContainer = document
+    // constante é que não muda, e dando um nome, criando um elemento chamado div, input, label, description
+    const taskContainer = document.createElement('div');
+    const newTask = document.createElement('input');
+    const taskLabel = document.createElement('label');
+    const taskDescriptionNode = document.createElement(description);
+
+    //no new task, que é o input, ele colocou dentro da tag os itens e os valores
+    newTask.setAttribute('type', 'checkbox');
+    newTask.setAttribute('name', description);
+    newTask.setAttribute('id', description)
     
+    // adicionou um task descriptionNODE no tasklabel
+    taskLabel.setAttribute('for', description);
+    taskLabel.appendChild(taskDescriptionNode);
     
-    
-    
-    
-    
-    
-    
-    res.innerHTML += `<p>
-        <input type="checkbox" class="checkbox" name="chk">
-        <label for="chk1">${itemf}</label>
-    </p>`;
+    //task container adicionou o item task item
+    //task container recebeu new task
+    //task container recebeu tasklabel
+    taskContainer.classList.add('task-item');
+    taskContainer.appendChild(newTask);
+    taskContainer.appendChild(taskLabel);
     
 
+    taskList.appendChild(taskContainer);
+    
+    
+   
+
+}
+*/
+form.onsubmit = function (e) {
+	e.preventDefault();
+	const inputField = document.getElementById('task-input');
+	addTask(inputField.value);
+	form.reset();
+};
+
+function addTask(description) {
+	const taskContainer = document.createElement('div');
+	const newTask = document.createElement('input');
+	const taskLabel = document.createElement('label');
+	const taskDescriptionNode = document.createTextNode(description);
+
+	newTask.setAttribute('type', 'checkbox');
+	newTask.setAttribute('name', description);
+	newTask.setAttribute('id', description);
+
+	taskLabel.setAttribute('for', description);
+	taskLabel.appendChild(taskDescriptionNode);
+
+	taskContainer.classList.add('res-item');
+	taskContainer.appendChild(newTask);
+	taskContainer.appendChild(taskLabel);
+
+	taskList.appendChild(taskContainer);
 }
