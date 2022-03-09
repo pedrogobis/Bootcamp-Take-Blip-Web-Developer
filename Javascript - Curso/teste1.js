f1b1a4b95d3f3fa6e1e124ab928f09b72449defb
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-03-09 11:11:54 -0300

    Fazendo um contador
    metodo de fazer um contador levemente diferente do guanabara.

---
+++ Javascript - Curso/teste1.js
@@ -0,0 +1,13 @@
+function returnEvenValues(array){
+    let evenNums = [];
+    for( let i = 0; i< array.lenght; i++){
+        if(array[i] % 2 === 0){
+            evenNums.push(array[i]);
+        }
+    }
+    console.log(evenNums);
+}
+
+let array = [1,2,3,4,5,6,7,8];
+
+returnEvenValues(array);
