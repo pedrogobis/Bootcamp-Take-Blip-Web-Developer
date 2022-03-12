c5a7932fde1ff7ff01392ace6a8889df593ceb17
Pedro Paulo Gobis <98911566+pedrogobis@users.noreply.github.com>
2022-03-12 02:48:25 -0300

    Create jgodefutebol.js

---
+++ Javascript - Curso/aula 4- estruturas condicionais/jgodefutebol.js
@@ -0,0 +1,13 @@
+var player1 = 3;
+var player2 = 6;
+var placar = 0;
+
+if (player1 != -1){
+    if (player1> 0){
+        console.log(`Jogador1 esta ganhando`)
+    }else if(player2>0) {
+        console.log('jogado2 esta ganhando')
+    }else {
+        console.log('ninguem marcou ponto')
+    }
+}
